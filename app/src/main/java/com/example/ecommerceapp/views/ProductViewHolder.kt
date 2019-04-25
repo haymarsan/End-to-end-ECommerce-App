15b6046bb0e6e5b6137a15c33201bc1259a5b106
Hay Mar San <haymarsan11@gmail.com>
2019-04-25 15:42:14 +0630

    First commit

---
+++ app/src/main/java/com/example/ecommerceapp/views/ProductViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceapp.views
+
+import android.support.v7.widget.RecyclerView
+import com.example.ecommerceapp.views.holders.BaseViewHolder
+
+class ProductViewHolder : BaseViewHolder {
+}
